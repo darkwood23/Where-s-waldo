742b38de1a2f7617a7510b4e4d8416d55de68ca3
Dark Wood <anonymoushackerintheweb@gmail.com>
2024-04-01 10:31:28 +0545

    Done making displayLeaderboards

--- src/components/displayLeaderboards.jsx
+++ src/components/displayLeaderboards.jsx
@@ -22,20 +22,26 @@ function DisplayLeaderBoards() {
             <h1>Leaderboards</h1>
 
             <div className="leaderboard">
-                <div className="titles">
-                    <h2 className="sn">S.N.</h2>
-                    <h2 className="username">Username</h2>
-                    <h2 className="score">Time</h2>
-                </div>
-                {leaderBoardArray.map((leaders, num) => {
-                    return (
-                        <div className="leader-element">
-                            <h3 className="sn-leaderboard">{num}</h3>
-                            <h3 className="leadername">{leaders.username}</h3>
-                            <h3 className="leader-score">{leaders.minutes}:{leaders.seconds}</h3>
-                        </div>
-                    )
-                })}
+                <table className="table">
+                    <thead>
+                        <tr className="titles">
+                            <th className="sn">S.N.</th>
+                            <th className="username">Username</th>
+                            <th className="score">Time</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-body">
+                        {leaderBoardArray.map((leaders, num) => {
+                            return (
+                                <tr className="leader-element" key={num}>
+                                    <td className="sn-leaderboard">{num + 1}</td>
+                                    <td className="leadername">{leaders.username}</td>
+                                    <td className="leader-score">{leaders.minutes}:{leaders.seconds}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
             </div>
             <Link to="/game-screen">
                 <button className="play-again">Play Again</button>
